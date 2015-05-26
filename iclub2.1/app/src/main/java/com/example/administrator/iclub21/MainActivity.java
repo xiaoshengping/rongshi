88799d576b34c6a901dbe0977412843ea3d1bba9
xiaoshengping <1277600115@qq.com>
2015-05-26 10:53:35 +0800

    第一次提交

--- iclub2.1/app/src/main/java/com/example/administrator/iclub21/MainActivity.java
+++ iclub2.1/app/src/main/java/com/example/administrator/iclub21/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inti();
+
     }
     public  void inti(){
         Intent intent=new Intent(this, HomeActivity.class);
